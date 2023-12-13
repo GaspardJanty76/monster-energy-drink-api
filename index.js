751a8448c4889c37fe8fb4afa29a309558211410
Gaspard Janty <144214357+GaspardJanty76@users.noreply.github.com>
2023-12-13 15:36:37 +0100

    V1

--- index.js
+++ index.js
@@ -3,23 +3,17 @@ import { monsters } from "./monster.js";
 import dotenv from "dotenv";
 import cors from "cors";
 
-
 dotenv.config();
-
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
 
+app.get('/monsters', (req, res) => {
+    res.json(monsters);
+});
 
-app.get('/monsters/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const monster = monsters.find(monster => monster.id === id);
-
-    res.json(monster)
-})
- 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-}) 
+});
